5b877ec0ec63004df8888b4e88833dc4df98abe4
Adam <adamoctav@gmail.com>
2021-04-16 01:19:04 +0300

    update showcase with card

--- src/app/modules/showcase/showcase.module.ts
+++ src/app/modules/showcase/showcase.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShowcaseComponent } from './showcase.component';
 import {ShowcaseRoutingModule} from './showcase-routing.module';
+import {SharedModule} from "../shared/shared.module";
 
 
 
 @NgModule({
   declarations: [ShowcaseComponent],
-  imports: [
-    CommonModule,
-    ShowcaseRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        ShowcaseRoutingModule,
+        SharedModule
+    ]
 })
 export class ShowcaseModule { }
