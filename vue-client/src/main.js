445f07d8f5ad7bdea8a6fd35da9d89c2a15dc83a
Adam <adamoctav@gmail.com>
2021-04-16 07:28:33 +0300

    add todo for future refs

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -11,3 +11,5 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+// TODO: Look for any missing things in the app @ https://github.com/adventurerlog/lbry-realm-client/tree/33abf5e638a535d3d5e6352b65fbc258a5c01dbd
