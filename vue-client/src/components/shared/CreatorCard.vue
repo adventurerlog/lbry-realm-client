21983d23e989de93fe5c4f301b4f68c77b8a9b97
Adam <adamoctav@gmail.com>
2021-04-16 05:08:44 +0300

    add shared components

---
+++ vue-client/src/components/shared/CreatorCard.vue
@@ -0,0 +1,17 @@
+<template>
+<p>creatorworks!</p>
+</template>
+
+<script>
+export default {
+  name: 'CreatorCard',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
